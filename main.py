12fb3c459a0efeb0dec3bfb8032da7a00ac6a192
Georg-JohannSeeder <Georg-Johann.Seeder@vikk.ee>
2023-02-22 10:00:24 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import pygame, sys
+
+pygame.init()
+
+# värvid
+red = [255, 0, 0]
+green = [0, 255, 0]
+blue = [0, 0, 255]
+pink = [255, 153, 255]
+lGreen = [153, 255, 153]
+lBlue = [153, 204, 255]
+
+# ekraani seaded
+screenX = 640
+screenY = 480
+screen = pygame.display.set_mode([screenX, screenY])
+pygame.display.set_caption("Animeerimine")
+screen.fill(lBlue)
+
+#graafika laadimine
+ralli = pygame.image.load("bg_rally.jpg")
+pauto = pygame.image.load("f1_red.png")
+sauto = pygame.image.load("f1_blue.png")
+
+#kiirus ja asukoht
+posX, posY = 0, 0
+speedX = 1
+
+gameover = False
+while not gameover:
+    #mängu sulgemine ristist
+    events = pygame.event.get()
+    for i in pygame.event.get():
+       if i.type == pygame.QUIT:
+           sys.exit()
+
+    #pildi lisamine ekraanile
+    screen.blit(ralli, (posX,posY))
+    screen.blit(pauto, (posX, posY))
+    screen.blit(sauto, (posX, posY))
+
+    posX += speedX
+
+    #graafika kuvamine ekraanil
+    pygame.display.flip()
+
+
+pygame.quit()
